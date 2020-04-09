233a3ad1a7a9380071a6efd54ce4843f2254402f
KUSKUS <reckplay1992@gmail.com>
2020-04-09 20:00:33 +0200

    Added ProceduralMesh dependency

--- Source/ProceduralTerrain/ProceduralTerrain.Build.cs
+++ Source/ProceduralTerrain/ProceduralTerrain.Build.cs
@@ -8,6 +8,6 @@ public class ProceduralTerrain : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "ProceduralMeshComponent" });
 	}
 }
