ea5963320529bd42018075a7bf070deddc326638
buliuszeg <66571381+buliuszeg@users.noreply.github.com>
2020-06-07 21:59:48 +0200

    Add files via upload

--- Source/ProceduralTerrain/private/PerlinNoise.cpp
+++ Source/ProceduralTerrain/private/PerlinNoise.cpp
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #define FASTFLOOR(x) ( ((x)>0) ? ((int)x) : (((int)x)-1) )
 #include "PerlinNoise.h"
@@ -14,9 +13,9 @@ PerlinNoise::~PerlinNoise()
 
 double _grad(int hash, double x, double y, double z)
 {
-	int h = hash & 15;									// Convert low 4 bits of hash code into 12 simple
-	double u = h < 8 ? x : y;							// gradient directions, and compute dot product.
-	double v = h < 4 ? y : h == 12 || h == 14 ? x : z;	// Fix repeats at h = 12 to 15
+	int h = hash & 15;									
+	double u = h < 8 ? x : y;							
+	double v = h < 4 ? y : h == 12 || h == 14 ? x : z;	
 	return ((h & 1) ? -u : u) + ((h & 2) ? -v : v);
 }
 
@@ -57,14 +56,14 @@ unsigned char perm[512] = { 151,160,137,91,90,15,
 double PerlinNoise::simplexNoise3D(double x, double y, double z)
 {
 
-	// Simple skewing factors for the 3D case
+	
 #define F3 0.3333333333333333333
 #define G3 0.1666666666666666667
 
-	double n0, n1, n2, n3; // Noise contributions from the four corners
+	double n0, n1, n2, n3; 
 
-						  // Skew the input space to determine which simplex cell we're in
-	double s = (x + y + z) * F3; // Very nice and simple skew factor for 3D
+						  
+	double s = (x + y + z) * F3; 
 	double xs = x + s;
 	double ys = y + s;
 	double zs = z + s;
@@ -73,54 +72,50 @@ double PerlinNoise::simplexNoise3D(double x, double y, double z)
 	int k = FASTFLOOR(zs);
 
 	double t = (double)(i + j + k) * G3;
-	double X0 = i - t; // Unskew the cell origin back to (x,y,z) space
+	double X0 = i - t; 
 	double Y0 = j - t;
 	double Z0 = k - t;
-	double x0 = x - X0; // The x,y,z distances from the cell origin
+	double x0 = x - X0; 
 	double y0 = y - Y0;
 	double z0 = z - Z0;
 
-	// For the 3D case, the simplex shape is a slightly irregular tetrahedron.
-	// Determine which simplex we are in.
-	int i1, j1, k1; // Offsets for second corner of simplex in (i,j,k) coords
-	int i2, j2, k2; // Offsets for third corner of simplex in (i,j,k) coords
+	
+	int i1, j1, k1; 
+	int i2, j2, k2; 
 
-					/* This code would benefit from a backport from the GLSL version! */
+					
 	if (x0 >= y0) {
 		if (y0 >= z0)
 		{
 			i1 = 1; j1 = 0; k1 = 0; i2 = 1; j2 = 1; k2 = 0;
-		} // X Y Z order
-		else if (x0 >= z0) { i1 = 1; j1 = 0; k1 = 0; i2 = 1; j2 = 0; k2 = 1; } // X Z Y order
-		else { i1 = 0; j1 = 0; k1 = 1; i2 = 1; j2 = 0; k2 = 1; } // Z X Y order
+		} 
+		else if (x0 >= z0) { i1 = 1; j1 = 0; k1 = 0; i2 = 1; j2 = 0; k2 = 1; } 
+		else { i1 = 0; j1 = 0; k1 = 1; i2 = 1; j2 = 0; k2 = 1; } 
 	}
-	else { // x0<y0
-		if (y0 < z0) { i1 = 0; j1 = 0; k1 = 1; i2 = 0; j2 = 1; k2 = 1; } // Z Y X order
-		else if (x0 < z0) { i1 = 0; j1 = 1; k1 = 0; i2 = 0; j2 = 1; k2 = 1; } // Y Z X order
-		else { i1 = 0; j1 = 1; k1 = 0; i2 = 1; j2 = 1; k2 = 0; } // Y X Z order
+	else { 
+		if (y0 < z0) { i1 = 0; j1 = 0; k1 = 1; i2 = 0; j2 = 1; k2 = 1; } 
+		else if (x0 < z0) { i1 = 0; j1 = 1; k1 = 0; i2 = 0; j2 = 1; k2 = 1; } 
+		else { i1 = 0; j1 = 1; k1 = 0; i2 = 1; j2 = 1; k2 = 0; } 
 	}
 
-	// A step of (1,0,0) in (i,j,k) means a step of (1-c,-c,-c) in (x,y,z),
-	// a step of (0,1,0) in (i,j,k) means a step of (-c,1-c,-c) in (x,y,z), and
-	// a step of (0,0,1) in (i,j,k) means a step of (-c,-c,1-c) in (x,y,z), where
-	// c = 1/6.
+	
 
-	double x1 = x0 - i1 + G3; // Offsets for second corner in (x,y,z) coords
+	double x1 = x0 - i1 + G3; 
 	double y1 = y0 - j1 + G3;
 	double z1 = z0 - k1 + G3;
-	double x2 = x0 - i2 + 2.0 * G3; // Offsets for third corner in (x,y,z) coords
+	double x2 = x0 - i2 + 2.0 * G3; 
 	double y2 = y0 - j2 + 2.0 * G3;
 	double z2 = z0 - k2 + 2.0 * G3;
-	double x3 = x0 - 1.0 + 3.0 * G3; // Offsets for last corner in (x,y,z) coords
+	double x3 = x0 - 1.0 + 3.0 * G3; 
 	double y3 = y0 - 1.0 + 3.0 * G3;
 	double z3 = z0 - 1.0 + 3.0 * G3;
 
-	// Wrap the integer indices at 256, to avoid indexing perm[] out of bounds
+	
 	int ii = i & 0xff;
 	int jj = j & 0xff;
 	int kk = k & 0xff;
 
-	// Calculate the contribution from the four corners
+	
 	double t0 = 0.6 - x0 * x0 - y0 * y0 - z0 * z0;
 	if (t0 < 0.0) n0 = 0.0;
 	else {
@@ -149,8 +144,7 @@ double PerlinNoise::simplexNoise3D(double x, double y, double z)
 		n3 = t3 * t3 * _grad(perm[ii + 1 + perm[jj + 1 + perm[kk + 1]]], x3, y3, z3);
 	}
 
-	// Add contributions from each corner to get the final noise value.
-	// The result is scaled to stay just inside [-1,1]
+	
 	return (32.0 * (n0 + n1 + n2 + n3)); // TODO: The scale factor is preliminary!
 }
 
